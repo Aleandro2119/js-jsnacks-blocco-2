aaf2c4f224ec52c3dd7f611f6923439d525dd267
Aleandro2119 <aleandro.basilico@gmail.com>
2022-01-13 17:42:12 +0100

    Add Snack 4

---
+++ js/script4.js
@@ -0,0 +1,19 @@
+// ? SNACK 4
+
+// Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+let numbers = ['30', '24', '27', '82', '59', '8', '16', '21', '44'];
+
+let i = 0;
+let sum = 0;
+
+while (i < numbers.length) {
+
+    sum = sum + numbers[i];
+
+    i = 2;
+
+}
+
+console.log(sum)
+
